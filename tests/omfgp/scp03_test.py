4465e1bf64a4c98f242f6bb0d4f969a6a6abad80
Mike Tolkachev <mstolkachev@gmail.com>
2021-01-26 12:48:30 +0300

    Refactor tests

--- tests/omfgp/scp03_test.py
+++ tests/omfgp/scp03_test.py
@@ -1,3 +1,4 @@
+from binascii import unhexlify as unhex
 import pytest
 import omfgp.scp
 from omfgp.scp03 import *
@@ -5,27 +6,27 @@ from omfgp.scp03 import _derive_data, _compute_block_size
 
 
 def test_derive_data():
-    key = bytes.fromhex("404142434445464748494A4B4C4D4E4F")
-    context = bytes.fromhex("68E9C8799FF8CB46F8D89D88BA1287BF")
+    key = unhex("404142434445464748494A4B4C4D4E4F")
+    context = unhex("68E9C8799FF8CB46F8D89D88BA1287BF")
     s_enc = _derive_data(key, DDC.S_ENC, len(key) * 8, context)
-    assert s_enc == bytes.fromhex("3C64070C595D1D72BDEE692DA99B7AE4")
+    assert s_enc == unhex("3C64070C595D1D72BDEE692DA99B7AE4")
     s_mac = _derive_data(key, DDC.S_MAC, len(key) * 8, context)
-    assert s_mac == bytes.fromhex("6E8468B74A707842E3D3696F338D2657")
+    assert s_mac == unhex("6E8468B74A707842E3D3696F338D2657")
     s_rmac = _derive_data(key, DDC.S_RMAC, len(key) * 8, context)
-    assert s_rmac == bytes.fromhex("FDD1D7C84750994815D0082734C31D88")
+    assert s_rmac == unhex("FDD1D7C84750994815D0082734C31D88")
     card_cryptogram = _derive_data(s_mac, DDC.CARD_CRYPTOGRAM, 0x0040, context)
-    assert card_cryptogram == bytes.fromhex("9B5C9D42D5059430")
+    assert card_cryptogram == unhex("9B5C9D42D5059430")
     host_cryptogram = _derive_data(s_mac, DDC.HOST_CRYPTOGRAM, 0x0040, context)
-    assert host_cryptogram == bytes.fromhex("F3C7624565B5894F")
+    assert host_cryptogram == unhex("F3C7624565B5894F")
 
 
 def test_derive_card_challenge():
-    key_enc = bytes.fromhex("404142434445464748494A4B4C4D4E4F")
-    sd_aid = bytes.fromhex("A000000018434D08090A0B0C000000")
-    seq_ctr = bytes.fromhex("00002A")
+    key_enc = unhex("404142434445464748494A4B4C4D4E4F")
+    sd_aid = unhex("A000000018434D08090A0B0C000000")
+    seq_ctr = unhex("00002A")
     context = seq_ctr + sd_aid
     card_challenge = _derive_data(key_enc, DDC.CARD_CHALLENGE, 0x0040, context)
-    assert card_challenge == bytes.fromhex("A3F5F144D19BE66E")
+    assert card_challenge == unhex("A3F5F144D19BE66E")
 
 
 def test_compute_block_size():
--- tests/omfgp/scp_test.py
+++ tests/omfgp/scp_test.py
@@ -1,3 +1,4 @@
+from binascii import unhexlify as unhex
 import pytest
 from pytest_mock import MockerFixture
 from unittest.mock import call
@@ -42,33 +43,33 @@ def test_scp03_clear(mocker: MockerFixture):
     # Security level: clear (no secure messaging)
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("2E6805A2847B9844"),
-            bytes.fromhex("00008048007283073469FF0300E4A2A0"
-                          "7404C25B33766379EE21A55CFA"), SUCCESS),
+            unhex("2E6805A2847B9844"),
+            unhex("00008048007283073469FF0300E4A2A0"
+                  "7404C25B33766379EE21A55CFA"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x00\x00': (
-            bytes.fromhex("684C48D43FEDEFD990252090A826F975"), b'', SUCCESS)
+            unhex("684C48D43FEDEFD990252090A826F975"), b'', SUCCESS)
     }, debug=True)
     progress_cb = mocker.stub()
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("2E6805A2847B9844"),
+        fake_card, host_challenge=unhex("2E6805A2847B9844"),
         progress_cb=progress_cb, security_level=SecurityLevel.CLEAR,
         block_size=213)
-    assert scp.version == 3
+    assert scp._VERSION == 3
     assert scp.block_size == 213
     progress_cb.assert_has_calls(
         [call(0), call(50), call(100)], any_order=False)
 
     # Wrap GET STATUS (0x80, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x80\x02' + card.encode("4F00")) ==
-            bytes.fromhex("80F28002024F00"))
-    response = (bytes.fromhex("E32A4F08A0000001510000009F700107"
-                              "C5039EFE80C407A0000000620001CE02"
-                              "0100CC08A000000151000000"), SUCCESS)
+            unhex("80F28002024F00"))
+    response = (unhex("E32A4F08A0000001510000009F700107"
+                      "C5039EFE80C407A0000000620001CE02"
+                      "0100CC08A000000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response
 
     # Wrap GET STATUS (0x40, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x40\x02' + card.encode("4F00")) ==
-            bytes.fromhex("80F24002024F00"))
+            unhex("80F24002024F00"))
     response = (b'', b'\x6A\x88')
     assert scp.unwrap_response(*response) == response
 
@@ -78,32 +79,32 @@ def test_scp03_C_MAC(mocker: MockerFixture):
     # Security level: C-MAC
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("68E9C8799FF8CB46"),
-            bytes.fromhex("00008048007283073469FF0300F8D89D"
-                          "88BA1287BF9B5C9D42D5059430"), SUCCESS),
+            unhex("68E9C8799FF8CB46"),
+            unhex("00008048007283073469FF0300F8D89D"
+                  "88BA1287BF9B5C9D42D5059430"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x01\x00': (
-            bytes.fromhex("F3C7624565B5894F16EF5681573C34A2"), b'', SUCCESS)
+            unhex("F3C7624565B5894F16EF5681573C34A2"), b'', SUCCESS)
     }, debug=True)
     progress_cb = mocker.stub()
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("68E9C8799FF8CB46"),
+        fake_card, host_challenge=unhex("68E9C8799FF8CB46"),
         progress_cb=progress_cb, block_size=213)
-    assert scp.version == 3
+    assert scp._VERSION == 3
     assert scp.block_size == 205
     progress_cb.assert_has_calls(
         [call(0), call(50), call(100)], any_order=False)
 
     # Wrap GET STATUS (0x80, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x80\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F280020A4F004C4C51B5DFA7A181"))
-    response = (bytes.fromhex("E32A4F08A0000001510000009F700107"
-                              "C5039EFE80C407A0000000620001CE02"
-                              "0100CC08A000000151000000"), SUCCESS)
+            unhex("84F280020A4F004C4C51B5DFA7A181"))
+    response = (unhex("E32A4F08A0000001510000009F700107"
+                      "C5039EFE80C407A0000000620001CE02"
+                      "0100CC08A000000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response
 
     # Wrap GET STATUS (0x40, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x40\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F240020A4F002108D3E0BA43B673"))
+            unhex("84F240020A4F002108D3E0BA43B673"))
     response = (b'', b'\x6A\x88')
     assert scp.unwrap_response(*response) == response
 
@@ -123,36 +124,36 @@ def test_scp03_C_DECRYPTION_C_MAC(mocker: MockerFixture):
     # Security level: C-DECRYPTION, C-MAC
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("CC8A0407F63066E8"),
-            bytes.fromhex("00008048007283073469FF0300EF23AA"
-                          "B4C343EDDE5B283674A8983749"), SUCCESS),
+            unhex("CC8A0407F63066E8"),
+            unhex("00008048007283073469FF0300EF23AA"
+                  "B4C343EDDE5B283674A8983749"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x03\x00': (
-            bytes.fromhex("112355D07180C24547A5EB3BD470F7C5"), b'', SUCCESS)
+            unhex("112355D07180C24547A5EB3BD470F7C5"), b'', SUCCESS)
     }, debug=True)
     progress_cb = mocker.stub()
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("CC8A0407F63066E8"),
+        fake_card, host_challenge=unhex("CC8A0407F63066E8"),
         progress_cb=progress_cb,
         security_level=SecurityLevel.C_DECRYPTION | SecurityLevel.C_MAC,
         block_size=213)
-    assert scp.version == 3
+    assert scp._VERSION == 3
     assert scp.block_size == 191
     progress_cb.assert_has_calls(
         [call(0), call(50), call(100)], any_order=False)
 
     # Wrap GET STATUS (0x80, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x80\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F28002184EE4C1127567676E7E0883"
-                          "3B785051FD96FDFE1EC5BD293F"))
-    response = (bytes.fromhex(
+            unhex("84F28002184EE4C1127567676E7E0883"
+                  "3B785051FD96FDFE1EC5BD293F"))
+    response = (unhex(
         "E32A4F08A0000001510000009F700107C5039EFE80C407A0000000620001CE02"
         "0100CC08A000000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response
 
     # Wrap GET STATUS (0x40, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x40\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F2400218E33CB507A6FFF09CE7A44E"
-                          "EB7498295D99F96B9F2470A782"))
+            unhex("84F2400218E33CB507A6FFF09CE7A44E"
+                  "EB7498295D99F96B9F2470A782"))
     response = (b'', b'\x6A\x88')
     assert scp.unwrap_response(*response) == response
 
@@ -162,47 +163,47 @@ def test_scp03_C_MAC_R_MAC(mocker: MockerFixture):
     # Security level: C-MAC, R-MAC
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("CB6DA009E0404A67"),
-            bytes.fromhex("0000013628140599538231037021428D"
-                          "A9468C2D5A8D05CE9657FBD5ED00001D"), SUCCESS),
+            unhex("CB6DA009E0404A67"),
+            unhex("0000013628140599538231037021428D"
+                  "A9468C2D5A8D05CE9657FBD5ED00001D"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x11\x00': (
-            bytes.fromhex("CC606C998B98BF8AF0746E4F50D3F65E"), b'', SUCCESS)
+            unhex("CC606C998B98BF8AF0746E4F50D3F65E"), b'', SUCCESS)
     }, debug=True)
     progress_cb = mocker.stub()
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("CB6DA009E0404A67"),
+        fake_card, host_challenge=unhex("CB6DA009E0404A67"),
         progress_cb=progress_cb,
         security_level=SecurityLevel.R_MAC | SecurityLevel.C_MAC,
         block_size=213)
-    assert scp.version == 3
+    assert scp._VERSION == 3
     assert scp.block_size == 205
     progress_cb.assert_has_calls(
         [call(0), call(50), call(100)], any_order=False)
 
     # Wrap GET STATUS (0x80, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x80\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F280020A4F002987243A9C15F44A"))
-    response = (bytes.fromhex(
+            unhex("84F280020A4F002987243A9C15F44A"))
+    response = (unhex(
         "E3264F08A0000001510000009F700101C5039EFE80C407A0000001515350CC08"
         "A0000001510000007D5F8BBC66683F42"), SUCCESS)
-    response_uw = (bytes.fromhex(
+    response_uw = (unhex(
         "E3264F08A0000001510000009F700101C5039EFE80C407A0000001515350CC08"
         "A000000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response_uw
 
     # Wrap GET STATUS (0x40, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x40\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F240020A4F00EB20CB18E1CE889F"))
+            unhex("84F240020A4F00EB20CB18E1CE889F"))
     response = (b'', b'\x6A\x88')
     assert scp.unwrap_response(*response) == response
 
     # Wrap GET STATUS (0x10, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x10\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F210020A4F00B334705EAFAB9FB1"))
-    response = (bytes.fromhex(
+            unhex("84F210020A4F00B334705EAFAB9FB1"))
+    response = (unhex(
         "E3254F07A00000015153509F700101CE02FFFF8408A000000151535041CC08A0"
         "000001510000004457D7641E95465F"), SUCCESS)
-    response_uw = (bytes.fromhex(
+    response_uw = (unhex(
         "E3254F07A00000015153509F700101CE02FFFF8408A000000151535041CC08A0"
         "00000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response_uw
@@ -213,51 +214,51 @@ def test_scp03_C_DECRYPTION_C_MAC_R_MAC(mocker: MockerFixture):
     # Security level: C-DECRYPTION, C-MAC, R-MAC
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("F70E1C5D67085E4B"),
-            bytes.fromhex("000001362814059953823103707B9EAD"
-                          "94A8B88028F47137A2FB093F5E000021"), SUCCESS),
+            unhex("F70E1C5D67085E4B"),
+            unhex("000001362814059953823103707B9EAD"
+                  "94A8B88028F47137A2FB093F5E000021"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x13\x00': (
-            bytes.fromhex("2E2057B873FCD25D175D4D63C5CD4CEB"), b'', SUCCESS)
+            unhex("2E2057B873FCD25D175D4D63C5CD4CEB"), b'', SUCCESS)
     }, debug=True)
     progress_cb = mocker.stub()
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("F70E1C5D67085E4B"),
+        fake_card, host_challenge=unhex("F70E1C5D67085E4B"),
         progress_cb=progress_cb,
         security_level=(SecurityLevel.C_DECRYPTION | SecurityLevel.C_MAC |
                         SecurityLevel.R_MAC),
         block_size=213)
-    assert scp.version == 3
+    assert scp._VERSION == 3
     assert scp.block_size == 191
     progress_cb.assert_has_calls(
         [call(0), call(50), call(100)], any_order=False)
 
     # Wrap GET STATUS (0x80, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x80\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F2800218E9170B7D79EA80AF507247"
-                          "375BB3F8EDE6F54FDD858BAE89"))
-    response = (bytes.fromhex(
+            unhex("84F2800218E9170B7D79EA80AF507247"
+                  "375BB3F8EDE6F54FDD858BAE89"))
+    response = (unhex(
         "E3264F08A0000001510000009F700101C5039EFE80C407A0000001515350CC08"
         "A000000151000000707D9895B36C6F6B"), SUCCESS)
-    response_uw = (bytes.fromhex(
+    response_uw = (unhex(
         "E3264F08A0000001510000009F700101C5039EFE80C407A0000001515350CC08"
         "A000000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response_uw
 
     # Wrap GET STATUS (0x40, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x40\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F240021804B77FFA640854E3BA8611"
-                          "F944C9B99F938E8E1A5FF70E98"))
+            unhex("84F240021804B77FFA640854E3BA8611"
+                  "F944C9B99F938E8E1A5FF70E98"))
     response = (b'', b'\x6A\x88')
     assert scp.unwrap_response(*response) == response
 
     # Wrap GET STATUS (0x10, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x10\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F2100218291B45B303C43B6222834B"
-                          "C0011A83A91BDD0CFD8BACD09B"))
-    response = (bytes.fromhex(
+            unhex("84F2100218291B45B303C43B6222834B"
+                  "C0011A83A91BDD0CFD8BACD09B"))
+    response = (unhex(
         "E3254F07A00000015153509F700101CE02FFFF8408A000000151535041CC08A0"
         "0000015100000039B68A29F735CFCC"), SUCCESS)
-    response_uw = (bytes.fromhex(
+    response_uw = (unhex(
         "E3254F07A00000015153509F700101CE02FFFF8408A000000151535041CC08A0"
         "00000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response_uw
@@ -268,52 +269,52 @@ def test_scp03_C_DECRYPTION_R_ENCRYPTION_C_MAC_R_MAC(mocker: MockerFixture):
     # Security level: C-DECRYPTION, R-ENCRYPTION, C-MAC, R-MAC
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("0C980267C298D194"),
-            bytes.fromhex("00000136281405995382310370730BE0"
-                          "14C03B4558A2155798D5B2C602000023"), SUCCESS),
+            unhex("0C980267C298D194"),
+            unhex("00000136281405995382310370730BE0"
+                  "14C03B4558A2155798D5B2C602000023"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x33\x00': (
-            bytes.fromhex("9716680D938E0CE764738F77E5E39408"), b'', SUCCESS)
+            unhex("9716680D938E0CE764738F77E5E39408"), b'', SUCCESS)
     }, debug=True)
     progress_cb = mocker.stub()
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("0C980267C298D194"),
+        fake_card, host_challenge=unhex("0C980267C298D194"),
         progress_cb=progress_cb,
         security_level=(
             SecurityLevel.C_DECRYPTION | SecurityLevel.R_ENCRYPTION |
             SecurityLevel.C_MAC | SecurityLevel.R_MAC),
         block_size=250)
-    assert scp.version == 3
+    assert scp._VERSION == 3
     assert scp.block_size == 239
     progress_cb.assert_has_calls(
         [call(0), call(50), call(100)], any_order=False)
 
     # Wrap GET STATUS (0x80, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x80\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F2800218AFB12C966FF538690FA041"
-                          "99550D8E93AF7F2569E92DEC66"))
-    response = (bytes.fromhex(
+            unhex("84F2800218AFB12C966FF538690FA041"
+                  "99550D8E93AF7F2569E92DEC66"))
+    response = (unhex(
         "0C7A219212D7B6E7B1F602BA291660368CA6E7C959E343FDBCDA20285A5A2A52"
         "143C8AA1ED5580A5DDB8A1D54730F461424520A6C56622FD"), SUCCESS)
-    response_uw = (bytes.fromhex(
+    response_uw = (unhex(
         "E3264F08A0000001510000009F700101C5039EFE80C407A0000001515350CC08"
         "A000000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response_uw
 
     # Wrap GET STATUS (0x40, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x40\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F240021851EF5596042E756C67E7A0"
-                          "31A33F88C25E0F05398B4ADFBA"))
+            unhex("84F240021851EF5596042E756C67E7A0"
+                  "31A33F88C25E0F05398B4ADFBA"))
     response = (b'', b'\x6A\x88')
     assert scp.unwrap_response(*response) == response
 
     # Wrap GET STATUS (0x10, 0x02) and unwrap response
     assert (scp.wrap_apdu(GET_STATUS + b'\x10\x02' + card.encode("4F00")) ==
-            bytes.fromhex("84F2100218CEB683FB79244285024670"
-                          "A969C17182CE2534871478DDA2"))
-    response = (bytes.fromhex(
+            unhex("84F2100218CEB683FB79244285024670"
+                  "A969C17182CE2534871478DDA2"))
+    response = (unhex(
         "123AB3A33146D67BC317E3873631F83DFC1C680D88E8147DD9CB28"
         "99433878847B911D7678CA6879E2FD674F02A778D56B563952AFA82611"), SUCCESS)
-    response_uw = (bytes.fromhex(
+    response_uw = (unhex(
         "E3254F07A00000015153509F700101CE02FFFF8408A000000151535041CC08A0"
         "00000151000000"), SUCCESS)
     assert scp.unwrap_response(*response) == response_uw
@@ -324,25 +325,25 @@ def test_scp03_Key_DEK():
     # Security level: clear (no secure messaging)
     fake_card = FakeCard({
         INITIALIZE_UPDATE + b'\x00\x00': (
-            bytes.fromhex("2E6805A2847B9844"),
-            bytes.fromhex("00008048007283073469FF0300E4A2A0"
-                          "7404C25B33766379EE21A55CFA"), SUCCESS),
+            unhex("2E6805A2847B9844"),
+            unhex("00008048007283073469FF0300E4A2A0"
+                  "7404C25B33766379EE21A55CFA"), SUCCESS),
         EXTERNAL_AUTHENTICATE + b'\x00\x00': (
-            bytes.fromhex("684C48D43FEDEFD990252090A826F975"), b'', SUCCESS)
+            unhex("684C48D43FEDEFD990252090A826F975"), b'', SUCCESS)
     }, debug=True)
     scp = open_secure_channel(
-        fake_card, host_challenge=bytes.fromhex("2E6805A2847B9844"),
+        fake_card, host_challenge=unhex("2E6805A2847B9844"),
         security_level=SecurityLevel.CLEAR)
-    assert scp.version == 3
+    assert scp._VERSION == 3
 
     # Test sensitive data encryption and decryption with Key-DEK
-    pt = bytes.fromhex("8B37F9148DF4BB25956BE6310C73C8DC"
-                       "58EA9714FF49B643107B34C9BFF096A9"
-                       "4FEDD6823526ABC27A8E0B16616EEE25"
-                       "4AB4567DD68E8CCD4C38AC563B13639C")
-    ct = bytes.fromhex("014E02F68B094BE48EE05AFC1B1A14D6"
-                       "193BDB0EDF8171115B6643A2EA4F1459"
-                       "9B9EE7FB0FF02C9B7FF297E96A0F4441"
-                       "D5B7ABC1380A54164D39F0004D4C8016")
+    pt = unhex("8B37F9148DF4BB25956BE6310C73C8DC"
+               "58EA9714FF49B643107B34C9BFF096A9"
+               "4FEDD6823526ABC27A8E0B16616EEE25"
+               "4AB4567DD68E8CCD4C38AC563B13639C")
+    ct = unhex("014E02F68B094BE48EE05AFC1B1A14D6"
+               "193BDB0EDF8171115B6643A2EA4F1459"
+               "9B9EE7FB0FF02C9B7FF297E96A0F4441"
+               "D5B7ABC1380A54164D39F0004D4C8016")
     assert scp.encrypt_data(pt) == ct
     assert scp.decrypt_data(ct) == pt
